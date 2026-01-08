2575fe4e1a30dfd8e4a9e077ed6a16e4bc019e7b
NoobinOut <110687587+NoobinOut@users.noreply.github.com>
2026-01-08 18:28:16 -0500

    Create GetOuttaTheWayAuto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/GetOuttaTheWayAuto.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+/** Autonomous that has us scoring then letting other robots score.
+ *  This auto has to do the following in this order:
+ *  Shoot 3 balls
+ *  Move 42 inches back and 46 inches right
+ *  open the ball release
+ *  auto complete
+
+ *  Hint: copy and pasting StarterBotAuto and changing the state machine in there is the way to go
+ * */
+public class GetOuttaTheWayAuto {
+}
